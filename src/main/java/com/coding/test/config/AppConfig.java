d3e2144fd9e4d05e380c7313879822ca23f9f258
Karthik Reddy <bobbili.kr@gmail.com>
2018-07-12 11:58:24 +0530

    Datsource for H2 is created

--- src/main/java/com/coding/test/config/AppConfig.java
+++ src/main/java/com/coding/test/config/AppConfig.java
@@ -1,12 +1,22 @@
 package com.coding.test.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages= {"com.coding.test"})
 public class AppConfig {
+	
+	@Bean
+	public DataSource getDataSource() {
+		return  new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+	}
 
 }
