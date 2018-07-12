f2385fc494731b114ca4b1fd435f70e1399b8fa7
Karthik Reddy <bobbili.kr@gmail.com>
2018-07-12 11:53:09 +0530

    Welcome Controller setup

---
+++ src/main/java/com/coding/test/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.coding.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@RequestMapping(value="/hello",method=RequestMethod.GET)
+	public String hello() {
+		return "Welcome to Coding Challenge";
+	}
+
+}
---
+++ src/main/java/com/coding/test/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.coding.test.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= {"com.coding.test"})
+public class AppConfig {
+
+}
